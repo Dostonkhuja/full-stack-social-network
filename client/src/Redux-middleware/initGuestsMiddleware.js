ae97a9a03da2688e79e98f91e9f9d4de5784596d
Dostonkhuja <77501580+Dostonkhuja@users.noreply.github.com>
2025-08-04 14:22:37 +0500

    Update initGuestsMiddleware.js

--- client/src/Redux-middleware/initGuestsMiddleware.js
+++ client/src/Redux-middleware/initGuestsMiddleware.js
@@ -2,7 +2,7 @@ import {createAsyncThunk} from "@reduxjs/toolkit";
 import createSocketIoMiddleware from 'redux-socket.io';
 import io from 'socket.io-client';
 
-let socket = io('https://social-beatles.herokuapp.com/guests',{autoConnect:false,reconnection: true,transports: ["websocket"]})
+let socket = io('https://social-beatles.onrender.com/guests',{autoConnect:false,reconnection: true,transports: ["websocket"]})
 export const GuestsSocketIoMiddleware = createSocketIoMiddleware(socket, "guests/")
 
 //server dispatch
